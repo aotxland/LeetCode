012d756940feb4d1f962320d6965b1648ca9f9ae
AoTxLand <aotxland@gmail.com>
2020-05-17 10:52:02 +0800

    add 2

---
+++ primary/String/IsPalindrome.java
@@ -0,0 +1,11 @@
+package String;
+
+/**
+ * @author AoTxLand
+ * @date 2020-05-17 10:51
+ */
+public class IsPalindrome {
+    public boolean isPalindrome(String s) {
+        return true;
+    }
+}
