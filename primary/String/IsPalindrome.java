531eeff5b6db5d81ba3929b66303af64513f7ce7
AoTxLand <aotxland@gmail.com>
2020-05-17 16:58:48 +0800

    string finish

--- primary/String/IsPalindrome.java
+++ primary/String/IsPalindrome.java
@@ -6,6 +6,33 @@ package String;
  */
 public class IsPalindrome {
     public boolean isPalindrome(String s) {
+        s = s.toLowerCase();
+        char[] chars = s.toCharArray();
+        int n = chars.length;
+        int a = 0;
+        int b = n - 1;
+        while(a < b){
+            if((chars[a] <= 'z' && chars[a] >= 'a') || chars[a] >= '0' && chars[a] <= '9'){
+                if((chars[b] <= 'z' && chars[b] >= 'a') || chars[b] >= '0' && chars[b] <= '9'){
+                    if(chars[a] == chars[b]){
+                        a++;
+                        b--;
+                    }
+                    else return false;
+                }
+                else{
+                    b--;
+                }
+            }
+            else{
+                a++;
+            }
+        }
         return true;
+    }
+
+    public static void main(String[] args) {
+        IsPalindrome ip = new IsPalindrome();
+        System.out.println(ip.isPalindrome("0p"));
     }
 }
---
+++ primary/String/LongestCommonPrefix.java
@@ -0,0 +1,25 @@
+package String;
+
+/**
+ * @author AoTxLand
+ * @date 2020-05-17 16:50
+ */
+public class LongestCommonPrefix {
+    public String longestCommonPrefix(String[] strs) {
+        int n = strs.length;
+        if(n == 0) return "";
+        if(n == 1) return strs[0];
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < strs[0].length(); i++){
+            char c = strs[0].charAt(i);
+            for(int j = 1; j < n; j++){
+                if(i > strs[j].length() - 1)
+                    return sb.toString();
+                if(strs[j].charAt(i) != c)
+                    return sb.toString();
+            }
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+}
