42aee2fc5c3a76f9b29b9dfe1fa31950de6898c5
AoTxLand <aotxland@gmail.com>
2020-05-17 09:30:09 +0800

    primary array finish

---
+++ Daily/ReverseKGroup.java
@@ -0,0 +1,65 @@
+package Daily;
+
+/**
+ * @author AoTxLand
+ * @date 2020-05-16 22:32
+ */
+
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+ }
+
+public class ReverseKGroup {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if (k==1||head==null)
+            return head;
+        return iterateHelper(head,k-1);
+    }
+
+    public ListNode iterateHelper(ListNode head,int k){
+        ListNode start=head,res=null;
+        ListNode pre=null;
+        while (true){
+            int index;
+            ListNode end=start;
+            for (index=0;index<k;++index){
+                if (end==null)
+                    break;
+                end=end.next;
+            }
+            if (index!=k||end==null)
+                break;
+            ListNode node=end==null?null:end.next;
+            end.next=null;
+            reverse(start);
+            if (pre!=null){
+                pre.next=end;
+            }else {
+                res=end;
+            }
+            pre=start;
+            start=node;
+        }
+        if (pre!=null)
+            pre.next=start;
+        return res;
+    }
+
+    /**
+     * 翻转链表
+     * @param head
+     */
+    public ListNode reverse(ListNode head){
+        ListNode pre=head,next=pre.next;
+        while (next!=null){
+            ListNode tmp=next.next;
+            next.next=pre;
+            pre=next;
+            next=tmp;
+        }
+        return head;
+    }
+}
---
+++ primary/Array/RotateImage.java
@@ -0,0 +1,27 @@
+package Array;
+
+/**
+ * @author AoTxLand
+ * @date 2020-05-17 9:12
+ */
+public class RotateImage {
+    public void rotate(int[][] matrix) {
+//        11->31 12->32 13->33
+//        21->12 22->22 23->32
+//        31->11 32->21 33->31
+        for(int i = 0; i < matrix.length; i++)
+            for(int j = i; j < matrix.length; j++){
+                int temp;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        for(int i = 0; i < matrix.length; i++)
+            for(int j = 0; j < matrix.length / 2; j++){
+                int temp;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length - j - 1];
+                matrix[i][matrix.length - j - 1] = temp;
+            }
+    }
+}
