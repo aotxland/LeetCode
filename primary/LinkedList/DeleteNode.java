a13f364c56e9ef883ed353dd1970e8c1543bdd25
AoTxLand <aotxland@gmail.com>
2020-05-17 17:17:13 +0800

    add LinkedList

---
+++ primary/LinkedList/DeleteNode.java
@@ -0,0 +1,12 @@
+package LinkedList;
+
+/**
+ * @author AoTxLand
+ * @date 2020-05-17 17:00
+ */
+
+public class DeleteNode {
+    public void deleteNode(ListNode node) {
+        node.next = node.next.next;
+    }
+}
