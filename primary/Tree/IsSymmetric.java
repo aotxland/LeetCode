2e8414d2d4a204534bf977f676bf8add21b457c1
AoTxLand <aotxland@gmail.com>
2020-05-20 22:11:51 +0800

    add 2

--- primary/Tree/IsSymmetric.java
+++ primary/Tree/IsSymmetric.java
@@ -8,12 +8,24 @@ import java.util.Stack;
  */
 public class IsSymmetric {
     public boolean isSymmetric(TreeNode root) {
-        Stack<TreeNode> stack = new Stack<>();
-        stack.push(root);
-        while(root == null || stack.isEmpty()){
-            TreeNode node = stack.pop();
-
-        }
+        if(root == null) return true;
+        if(root.left !=null && root.right != null)
+            return nodeSymmetric(root.left,root.right);
+        else if(root.right == root.left)
+            return true;
         return false;
+    }
+    public boolean nodeSymmetric(TreeNode t1,TreeNode t2) {
+        if (t1.val == t2.val) {
+            boolean flag = true;
+            if (t1.right != null && t2.left != null)
+                flag = nodeSymmetric(t1.right, t2.left);
+            else if(t1.right != t2.left) return false;
+            if (!flag) return false;
+            if (t1.left != null && t2.right != null)
+                flag = nodeSymmetric(t1.left, t2.right);
+            else if(t1.left != t2.right) return false;
+            return flag;
+        } else return false;
     }
 }
