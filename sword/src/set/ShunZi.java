9e5fd17f160c88dd402eb7d82a988bfce65d7c33
AoTxLand <aotxland@gmail.com>
2020-08-20 20:04:54 +0800

    61

--- sword/src/set/ShunZi.java
+++ sword/src/set/ShunZi.java
@@ -1,11 +1,19 @@
 package set;
 
+import java.util.Arrays;
+
 /**
  * @author AoTxLand
  * @date 2020-08-20 19:53
  */
 public class ShunZi {
     public boolean isStraight(int[] nums) {
-
+        int joker = 0;
+        Arrays.sort(nums); // 数组排序
+        for(int i = 0; i < 4; i++) {
+            if(nums[i] == 0) joker++; // 统计大小王数量
+            else if(nums[i] == nums[i + 1]) return false; // 若有重复，提前返回 false
+        }
+        return nums[4] - nums[joker] < 5; // 最大牌 - 最小牌 < 5 则可构成顺子
     }
 }
